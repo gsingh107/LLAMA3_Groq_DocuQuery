c7c51445674d1723118dcf0c722f3f71df9447f5
gsingh107 <102300799+gsingh107@users.noreply.github.com>
2024-07-28 22:29:46 +0530

    Added ChatGPT like UI for queries

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ import os
 from sentence_transformers import SentenceTransformer
 import time
 from langchain.embeddings import HuggingFaceEmbeddings
-# from HTMLtemplates import css
+from HTMLtemplates import css
+import gc
 
 def get_pdf_docs(pdfs):
     text= ""
@@ -56,7 +57,7 @@ def chat_groq(messages):
             response_content += chunk.choices[0].delta.content
     return response_content
 st.set_page_config(page_title='Chat with Multiple PDFs',page_icon=':books:')
-# st.write(css, unsafe_allow_html=True)
+st.write(css, unsafe_allow_html=True)
 
 def summarize_chat_history(chat_history):
     chat_history_text = " ".join([f"{chat['role']}: {chat['content']}" for chat in chat_history])
@@ -66,14 +67,28 @@ def summarize_chat_history(chat_history):
     summary = chat_groq(messages)
     return summary
 
+def reset_chat():
+    st.session_state.user_input = ""
+    st.session_state.conversation = None
+    st.session_state.current_prompt = ""
+
+    # st.session_state.chat_summary = ""
+    st.session_state.chat_history = []
+    st.session_state.messages = []
+    gc.collect()
 
 def main():
     load_dotenv()
+    col1, col2 = st.columns([6, 1])
 
-    st.markdown('<h1 class="main-header">Chat with Multiple PDFs 📚 </h1>', unsafe_allow_html=True)
-    st.markdown('<p class="sub-header">Ask questions about your documents:</p>', unsafe_allow_html=True)
-    user_input = st.text_area('', key="user_input", placeholder="Type your question here..."
-                                  )
+    with col1:
+        st.markdown('<h1 class="main-header">Chat with Multiple PDFs 📚 </h1>', unsafe_allow_html=True)
+
+    with col2:
+        st.button("Clear ↺", on_click=reset_chat)
+       # st.markdown('<p class="sub-header">Ask questions about your documents:</p>', unsafe_allow_html=True)
+    # Accept user input
+
 
 
     # st.session_state.user_input = st.text_input('Ask questions about your documents:')
@@ -93,7 +108,8 @@ def main():
         st.session_state.chat_summary = ""
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
-
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
 
     with st.sidebar:
@@ -114,50 +130,73 @@ def main():
                     vectorstore = get_vectorstores(chunks)
                     st.session_state.vectorstore = vectorstore
                 # create vector database
+    for message in st.session_state.chat_history:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 
-    user_question = st.session_state.user_input
+    if user_input := st.chat_input("Ask any question about your dcouments...."):
+        st.session_state.messages.append({"role": "user", "content": user_input})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(user_input)
+
+    # user_question = st.session_state.user_input
     # st.write(user_question)
-    if st.session_state.vectorstore is not None:
-        def submit_with_pdf():
+    # st.write(user_question)
+    if user_input is not None and st.session_state.vectorstore is not None:
 
-            retriever = st.session_state.vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
-            context = retriever.invoke(user_question)
-            prompt = f'''
-                        Answer the user's question based on the latest input provided in the chat history. Ignore
-                        previous inputs unless they are directly related to the latest question. Provide a generic
-                        answer if the answer to the user's question is not present in the context by mentioning it
-                        as general information.
-    
-                        Context: {context}
-    
-                        Chat History: {st.session_state.chat_history}
-    
-                        Latest Question: {user_question}
-                        '''
 
-            messages = [{'role': 'system', 'content': 'You are a very helpful assistant'}]
-            messages.append({'role': 'user', 'content': prompt})
 
-            try:
-                ai_response = chat_groq(messages)
-            except Exception as e:
-                st.error(f"Error occurred during chat_groq execution: {str(e)}")
-                ai_response = "An error occurred while fetching response. Please try again."
+        message_placeholder = st.empty()
+        full_response = ""
 
-            # Display the current output prompt
-            st.session_state.current_prompt = ai_response
+        retriever = st.session_state.vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+        context = retriever.invoke(user_input)
+        prompt = f'''
+                    Answer the user's question based on the latest input provided in the chat history. Ignore
+                    previous inputs unless they are directly related to the latest question. Provide a generic
+                    answer if the answer to the user's question is not present in the context by mentioning it
+                    as general information.
 
-            # Update chat history
-            st.session_state.chat_history.append({'role': 'user', 'content': user_question})
-            st.session_state.chat_history.append({'role': 'assistant', 'content': ai_response})
+                    Context: {context}
 
-            # Clear the input field
-            st.session_state.user_input = ""
+                    Chat History: {st.session_state.chat_history}
 
-    def submit_without_pdf():
+                    Latest Question: {user_input}
+                    '''
 
-        if user_question:
+        messages = [{'role': 'system', 'content': 'You are a very helpful assistant'}]
+        messages.append({'role': 'user', 'content': prompt})
+
+        try:
+            ai_response = chat_groq(messages)
+        except Exception as e:
+            st.error(f"Error occurred during chat_groq execution: {str(e)}")
+            ai_response = "An error occurred while fetching response. Please try again."
+
+        # Display the current output prompt
+        # st.session_state.current_prompt = ai_response
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            for chunk in ai_response:
+                full_response += chunk
+                message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+
+        # Update chat history
+        st.session_state.chat_history.append({'role': 'user', 'content': user_input})
+        st.session_state.chat_history.append({'role': 'assistant', 'content': ai_response})
+
+        # Clear the input field
+        st.session_state.user_input = ""
+
+    else:
+
+
+        if user_input:
             prompt = f'''
             Answer the user's question based on the latest input provided in the chat history. Ignore
             previous inputs unless they are directly related to the latest
@@ -165,60 +204,53 @@ def main():
 
             Chat History: {st.session_state.chat_history}
 
-            Latest Question: {user_question}
+            Latest Question: {user_input}
             '''
 
-            messages = [{'role': 'system', 'content': 'You are a very helpful assistant'}]
-            messages.append({'role': 'user', 'content': prompt})
+            # messages = [{'role': 'system', 'content': 'You are a very helpful assistant'}]
+            st.session_state.messages.append({'role': 'user', 'content': prompt})
 
             try:
-                ai_response = chat_groq(messages)
+                ai_response = chat_groq(st.session_state.messages)
             except Exception as e:
                 st.error(f"Error occurred during chat_groq execution: {str(e)}")
                 ai_response = "An error occurred while fetching response. Please try again."
 
-            # Display the current output prompt
-            st.session_state.current_prompt = ai_response
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                full_response = ""
+                for chunk in ai_response:
+                    full_response += chunk
+                    message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+
+
 
             # Update chat history
-            st.session_state.chat_history.append({'role': 'user', 'content': user_question})
+            st.session_state.chat_history.append({'role': 'user', 'content': user_input})
             st.session_state.chat_history.append({'role': 'assistant', 'content': ai_response})
 
-            # Clear the input field
-            st.session_state.user_input = ""
-    # Display the current output prompt if available
 
 
 
-    if st.session_state.vectorstore is not None:
-        st.button('Submit', on_click=submit_with_pdf)
-    else:
-        st.button('Submit', on_click=submit_without_pdf)
 
     # Display the current output prompt if available
     if st.session_state.current_prompt:
         st.write(st.session_state.current_prompt)
 
     # Button to generate chat summary
-    if st.button('Generate Chat Summary'):
-        st.session_state.chat_summary = summarize_chat_history(st.session_state.chat_history)
+
+    generate_summary_disabled = len(st.session_state.chat_history) == 0
+    st.button('Generate Chat Summary', disabled=generate_summary_disabled,
+              on_click=lambda: summarize_chat_history(st.session_state.chat_history))
+
 
     # Display the chat summary if available
     if st.session_state.chat_summary:
         with st.expander("Chat Summary"):
             st.write(st.session_state.chat_summary)
 
-    # Display the last 4 messages in an expander
-    with st.expander("Recent Chat History"):
-        recent_history = st.session_state.chat_history[-8:][::-1]
-        reversed_history = []
-        for i in range(0, len(recent_history), 2):
-            if i+1 < len(recent_history):
-                reversed_history.extend([recent_history[i+1], recent_history[i]])
-            else:
-                reversed_history.append(recent_history[i])
-        for chat in reversed_history:
-            st.write(f"{chat['role'].capitalize()}: {chat['content']}")
+
 
 
 if __name__ =='__main__':
